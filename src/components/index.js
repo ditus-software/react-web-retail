5f514717dacda7cfd9a606ddb93ca6ff21988d0c
Andrew McKendrick <ditus@rocketmail.com>
2023-05-02 10:15:44 -0400

    Exports SaveCoupon component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Price } from './price/price';
 export { default as Sponsored } from './sponsored/sponsored';
 export { default as ChokingHazard } from './choking-hazard/choking-hazard';
 export { default as LowStock } from './low-stock/low-stock';
+export { default as SaveCoupon } from './save-coupon/save-coupon';
